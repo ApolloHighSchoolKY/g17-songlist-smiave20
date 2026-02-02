08f3f6c911baaed7451f2ad6c4e86df6da12de83
smiave20 <avery.smith@stu.daviess.kyschools.us>
2026-02-02 18:13:40 +0000

    Commit

--- MusicDownloads.java
+++ MusicDownloads.java
@@ -7,13 +7,23 @@ public class MusicDownloads{
   public DownloadInfo getDownloadInfo(String title)
   {
     /* To be implemented in part (a) */
+    int count = 0;
 
+    for(int i = 0; i > downloadList.size()-1; i++)
+      if(DownloadInfo.equals(title))
+        count++;
+      else
+        return null;
+    
+    return count;
 
   }
 
   public void updateDownloadInfo(List<String> titles)
   {
     /* To be implemented in part (b) */
+    
+    for(DownloadInfo x: downloadList)
 
 
 
